81123eadf610ceb569d114ea22e49fb1efc1dd63
avshesh savani <avshesh.savani7@gmail.com>
2025-06-21 07:10:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- workspace/src/lib/firebase-config.ts
+++ workspace/src/lib/firebase-config.ts
@@ -7,9 +7,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyBKTFw_nmwW1R_KXzpzyiGRKaFI0fxCDfg",
   authDomain: "noteboard-13de1.firebaseapp.com",
   projectId: "noteboard-13de1",
-  storageBucket: "noteboard-13de1.appspot.com",
+  storageBucket: "noteboard-13de1.firebasestorage.app",
   messagingSenderId: "874285693727",
   appId: "1:874285693727:web:5eaee38fe5a977bac70e2a",
+  measurementId: "G-1QEEC4L0SL"
 };
 
 // Initialize Firebase
